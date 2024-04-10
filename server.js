ed8503b9ac6e70dd71cad94299d3151f6792c97a
FabioCorreiaLima <fabiocorreia455@gmail.com>
2024-04-09 21:18:55 -0300

    adicionando função para deletar

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+// Importando o app do arquivo app.js dentro do diretório src
 import app from './src/app.js'
 
+// Definindo a porta em que o servidor
 const PORT = 3000
+
+// Iniciando o servidor Express para escutar na porta especificada
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-  })
+    // Exibindo uma mensagem no console quando o servidor estiver em execução
+    console.log(`https://localhost${PORT}`)
+})
--- src/app.js
+++ src/app.js
@@ -1,41 +1,62 @@
+// Importando o módulo Express
+import express, { json } from 'express'
 
-import express from 'express'
-
+// Criando uma instância do Express
 const app = express()
 
-
+// Middleware para processar dados JSON
 app.use(express.json())
+
+// Array de seleções (exemplo de dados)
 const selecoes = [
-        {id : 1, selecao : 'Brasil', grupo : 'G'},
-        {id : 2, selecao : 'servia', grupo : 'G'},
-        {id : 3, selecao : 'camaroes', grupo : 'G'},
-        {id : 4, selecao : 'argentina', grupo : 'G'}
+    { id: 1, selecao: 'Brasil', grupo: 'G' },
+    { id: 2, selecao: 'Servia', grupo: 'G' },
+    { id: 3, selecao: 'Camarões', grupo: 'G' },
+    { id: 4, selecao: 'Argentina', grupo: 'G' }
 ]
 
-function BuscarSelecaoPorId(id){
+// Função para buscar uma seleção por ID
+function BuscarSelecaoPorId(id) {
+    // Filtrando o array de seleções pelo ID fornecido
     return selecoes.filter(selecao => selecao.id == id)
 }
 
 
+function DeletarSelecaoPorIndex(id) {
+    // Filtrando o array de seleções pelo ID fornecido
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
+// Rota para a página inicial
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
+// Rota para buscar uma seleção pelo ID
 app.get('/selecoes/:id', (req, res) => {
-    // let index = req.params.id
+    // Chamando a função BuscarSelecaoPorId e enviando a resposta como JSON
     res.json(BuscarSelecaoPorId(req.params.id))
+})
+
+// Rota para listar todas as seleções
+app.get('/selecoes', (req, res) => {
+    // Enviando o array de seleções como resposta
+    res.status(200).send(selecoes)
+})
+
+// Rota para cadastrar uma nova seleção
+app.post('/selecoes', (req, res) => {
+    // Adicionando a nova seleção ao array selecoes
+    selecoes.push(req.body)
+    // Respondendo com status 201 (Created) e uma mensagem
+    res.status(201).send('Seleção cadastrada com sucesso!')
+})
+
+app.delete('/selecoes/delete/:id', (req, res) => {
+    let index = DeletarSelecaoPorIndex (req.params.id)
+    selecoes.splice(index,1)
     console.log(index)
 })
 
 
-app.get('/selecoes', (req, res) => {
-    res.status(200).send(selecoes)
-
-})
-
-app.post('/selecoes', (req, res) =>{
-    selecoes.push(req.body)
-    res.status(201).send('seleção cadastrada com sucesso!')
-})
-
+// Exportando o app para uso em outros módulos
 export default app
